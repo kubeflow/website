a438c16b70d531f017139e4a7699c9d4f01e1230
Siddika1278 <72142188+Siddika1278@users.noreply.github.com>
2020-10-30 09:38:25 +0530

    Create Prime.c

---
+++ Prime.c
@@ -0,0 +1,40 @@
+// C program to check if a 
+// number is prime 
+  
+#include <stdio.h> 
+#include<math.h> 
+int main() 
+{ 
+    int n, i, flag = 1; 
+  
+    // Ask user for input 
+    printf("Enter a number: \n"); 
+  
+    // Store input number in a variable 
+    scanf("%d", &n); 
+  
+    // Iterate from 2 to n/2 
+    for (i = 2; i <= sqrt(n); i++) { 
+  
+        // If n is divisible by any number between 
+        // 2 and n/2, it is not prime 
+        if (n % i == 0) { 
+            flag = 0; 
+            break; 
+        } 
+    } 
+  
+    if(n<=1) 
+    flag=0; 
+    else if(n==2) 
+    flag=1;  
+  
+    if (flag == 1) { 
+        printf("%d is a prime number", n); 
+    } 
+    else { 
+        printf("%d is not a prime number", n); 
+    } 
+  
+    return 0; 
+} 
